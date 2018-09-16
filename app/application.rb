ee6354dfa06710b4a9655eabfa3fd86376ad9dd1
louis baptiste <louisvis@ymail.com>
2018-09-16 07:47:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/songs/)
+    if req.path.match(/items/)
 
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
+      item_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|element| element.title == item_title}
 
-      resp.write song.artist
+      resp.write item
     end
-
+else
+  resp.write "404"
+end
     resp.finish
   end
 end
