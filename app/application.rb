b75fe6f0be2989a7c9cfb5631a6bde85a12b86df
louis baptiste <louisvis@ymail.com>
2018-09-16 08:14:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,26 +7,18 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-
-      item_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-      item = @@items.find{|element| element.name == item_title}
-
-      resp.write item.price
-
-  else
-    resp.write "Dont have this item in our cart"
-    resp.status = 400
-  end
-  
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
     else
+      resp.status=404
       resp.write "Route not found"
-      resp.status = 404
     end
-
     resp.finish
   end
-
-
-
 
 end
